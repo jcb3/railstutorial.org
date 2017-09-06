f17ead852ee89155045da1b2a8bd58922b28d8ad
jaycb3 <jaycb3rd@gmail.com>
2017-09-06 10:32:55 -0500

    Add gravatar_for helper.  Add sho

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def delete
+  end
+
+  def create
+  end
+
+
 end
